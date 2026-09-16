ecfd48db8be20976bf15c3bf4d6b6c63474a1c87
royrizwancs-dev <royrizwancs@gmail.com>
2026-09-16 15:45:18 +0500

    lab2task added

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.id = 101;
+        s1.name = "Rizwan";
+        s1.completeCredits = 18;
+
+        s1.printSummary();
+    }
+}
---
+++ Student.java
@@ -0,0 +1,11 @@
+class Student {
+    int id;
+    String name;
+    int completeCredits;
+
+    void printSummary() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Complete Credits: " + completeCredits);
+    }
+}
